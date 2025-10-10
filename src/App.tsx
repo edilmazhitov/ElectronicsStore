7b8d969edf3f2eb30f5307806ed8b9c761528c28
edilmazhitov <zusupovaerkinai@gmail.com>
2025-10-10 10:19:13 +0300

    start projects

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+ const App = () => {
+
+  return (
+    <h1>START!!!</h1>
+  )
+ }
+
+ 
+ export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import Container from "../Container/Container"
+import styles from "./Header.module.scss"
+
+
+const Header = () => {
+
+    return (
+        <header className={styles.header}>
+            <Container>
+
+            </Container>
+        </header>   
+    )
+}
+
+export default Header
