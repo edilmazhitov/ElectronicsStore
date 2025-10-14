b25b9684e26fe9c80e31417a2e1bd7dc15d9d0b2
edilmazhitov <zusupovaerkinai@mail.com>
2025-10-14 22:37:52 +0600

    add home pages interesting and descounts sections

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,10 @@ import SwiperImage from '@/components/SwiperImage/SwiperImage.tsx'
 import styles from './Home.module.scss'
 import HomeNavbar from '@/components/HomeNavbar/HomeNavbar.tsx'
 
+import { CiCreditCard1 ,CiDiscount1} from "react-icons/ci";
+import { SlSocialDropbox } from "react-icons/sl";
+import { TbListDetails } from "react-icons/tb";
+
 const Home = () => {
   return (
     <main className="main">
@@ -11,9 +15,88 @@ const Home = () => {
           <SwiperImage />
         </Container>
       </section>
+      <section className={styles['home-interesting']}>
+        <Container>
+            <ul className={styles['home-interesting__list']}>
+              <li className={styles['home-interesting__list-item']}>
+                <CiCreditCard1 className={styles['home-interesting__icon']} />
+                <p className={styles['home-interesting__text']}>
+                  Оплата любым удобным способом
+                </p>
+              </li>
+              <li className={styles['home-interesting__list-item']}>
+                <TbListDetails className={styles['home-interesting__icon']} />
+                <p className={styles['home-interesting__text']}>
+                  Большой выбор товаров в каталоге
+                </p>
+              </li>
+              <li className={styles['home-interesting__list-item']}>
+                <SlSocialDropbox className={styles['home-interesting__icon']} />
+                <p className={styles['home-interesting__text']}>
+                  Осуществляем быструю доставку
+                </p>
+              </li>
+              <li className={styles['home-interesting__list-item']}>
+                <CiDiscount1 className={styles['home-interesting__icon']} />
+                <p className={styles['home-interesting__text']}>
+                  Делаем скидки на крупные покупки
+                </p>
+              </li>
+            </ul>
+        </Container>
+      </section>
       <section className={styles['home-navbar']}>
         <Container>
           <HomeNavbar />
+        </Container>
+      </section>
+      <section className={styles['home-discounts']}>
+        <Container>
+          <div className={styles['home-discounts__block']}>
+            <div className={styles['home-discounts__box']}>
+              <h2  className={styles['home-discounts__box-title']}>
+                Метизные <br/>
+                изделия
+              </h2>
+              <div  className={styles['home-discounts__box-text']}>
+                <p>
+                  до -15%
+                </p>
+              </div>
+            </div>
+            <div className={styles['home-discounts__box']}>
+              <h2  className={styles['home-discounts__box-title']}>
+                Лакокрасочные материалы
+              </h2>
+              <div  className={styles['home-discounts__box-text']}>
+                <p>
+                  до -30%
+                </p>
+              </div>
+            </div>
+            <div className={styles['home-discounts__box']}>
+              <h2  className={styles['home-discounts__box-title']}>
+                Напольные <br/>
+                покрытия
+              </h2>
+              <div  className={styles['home-discounts__box-text']}>
+                <p>
+                  до -25%
+                </p>
+              </div>
+            </div>
+            <div className={styles['home-discounts__box']}>
+              <h2  className={styles['home-discounts__box-title']}>
+                Все для <br/>
+                отоплления
+              </h2>
+              <div  className={styles['home-discounts__box-text']}>
+                <p>
+                  до -30%
+                </p>
+              </div>
+            </div>
+          </div>
         </Container>
       </section>
     </main>
