adb6e3d36657a9f2bd3397fb8f0fe2e1761604ee
edilmazhitov <zusupovaerkinai@gmail.com>
2025-10-23 12:03:32 +0600

    finish register pages styles

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -3,10 +3,17 @@ import { useState } from 'react'
 import type { IUser } from '@/types/userTypes.ts'
 import { useAppDispatch } from '@/hooks/reduxHooks.ts'
 import { registerUser } from '@/store/reducers/user.ts'
+import styles from './Register.module.scss'
+import Container from '@mui/material/Container'
 
 const Register = () => {
   const [userId, setUserId] = useState<number>(1)
-  const { register, handleSubmit, getValues } = useForm<IUser>({
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<IUser>({
     mode: 'onBlur',
   })
 
@@ -30,116 +37,180 @@ const Register = () => {
   }
 
   return (
-    <>
-      <form action="" onSubmit={handleSubmit(registerSubmit)}>
-        <div>
-          <label htmlFor="fullname"></label>
-          <input
-            id="fullname"
-            {...register('fullName', {
-              required: {
-                message: 'Эл. почта обязательна',
-                value: true,
-              },
-              minLength: {
-                message: 'Минимум 10 символов',
-                value: 10,
-              },
-              pattern: {
-                value: /^[A-Za-zА-Яа-яЁё]+(?:\s+[A-Za-zА-Яа-яЁё]+)+$/,
-                message:
-                  'Имя и фамилия должны состоять из букв и хотя бы из двух слов',
-              },
-            })}
-            placeholder="fullname"
-            type="text"
-          />
+    <div className={styles.register}>
+      <Container>
+        <h1 className={styles.register__title}>Регистрация</h1>
 
-          <label htmlFor="email"></label>
-          <input
-            id="email"
-            {...register('email', {
-              required: {
-                message: 'Эл. почта обязательна',
-                value: true,
-              },
-              minLength: {
-                message: 'Минимум 10 символов',
-                value: 10,
-              },
-              pattern: {
-                message: 'Неправильный формат email',
-                value: /^[^ ]+@[^ ]+\.[a-z]{2,5}$/,
-              },
-            })}
-            placeholder="Эл.почта"
-            type="email"
-            autoComplete="email"
-          />
-        </div>
-        <label htmlFor="tel"></label>
-        <input
-          {...register('phoneNumber', {
-            required: {
-              message: 'Эл. почта обязательна',
-              value: true,
-            },
-          })}
-          placeholder="Telephone"
-          type="number"
-          id="tel"
-        />
-        <label htmlFor="region"></label>
-        <input
-          {...register('region', {
-            required: {
-              message: 'Эл. почта обязательна',
-              value: true,
-            },
-          })}
-          placeholder="region"
-          type="text"
-          id="region"
-        />
-        <div className="register__block">
-          <input
-            {...register('password', {
-              required: {
-                message: 'Пароль обязателен',
-                value: true,
-              },
-              pattern: {
-                value:
-                  /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g,
-                message:
-                  'Пароль должен содержать минимум 8 символов, заглавную букву, число!',
-              },
-            })}
-            placeholder="Придумайте пароль"
-            type={'password'}
-            autoComplete="new-password"
-          />
-        </div>
+        <h2 className={styles.error}>
+          Ошибка: <br />
+          {errors.email?.message} <br />
+          {errors.password?.message} <br />
+          {errors.confirmPassword?.message}
+          <br />
+          {errors.phoneNumber?.message}
+          <br />
+          {errors.region?.message}
+        </h2>
+        <div className={styles.register__block}>
+          <form
+            action=""
+            className={styles.register__form}
+            onSubmit={handleSubmit(registerSubmit)}
+          >
+            <div className={styles['register__form-top']}>
+              <div className={styles['register__form-left']}>
+                <label
+                  htmlFor="email"
+                  className={styles['register__form-label']}
+                >
+                  Email <span>*</span>:
+                </label>{' '}
+                <br />
+                <input
+                  className={styles['register__form-input-top']}
+                  id="email"
+                  {...register('email', {
+                    required: {
+                      message: 'Эл. почта обязательна',
+                      value: true,
+                    },
+                    minLength: {
+                      message: 'Минимум 10 символов в эл. почте',
+                      value: 10,
+                    },
+                    pattern: {
+                      message: 'Неправильный формат email',
+                      value: /^[^ ]+@[^ ]+\.[a-z]{2,5}$/,
+                    },
+                  })}
+                  placeholder="Эл.почта"
+                  type="email"
+                  autoComplete="email"
+                />
+              </div>
 
-        <div className="register__block">
-          <input
-            {...register('confirmPassword', {
-              required: {
-                message: 'Подтвердите пароль',
-                value: true,
-              },
-              validate: (value) =>
-                value === getValues('password') || 'Пароли не совпадают',
-            })}
-            placeholder="Повторите пароль"
-            type="password"
-            autoComplete="new-password"
-          />
+              <div className={styles['register__form-right']}>
+                <label htmlFor="tel" className={styles['register__form-label']}>
+                  Номер телефона <span>*</span>:
+                </label>{' '}
+                <br />
+                <input
+                  className={styles['register__form-input-top']}
+                  {...register('phoneNumber', {
+                    required: {
+                      message: 'Номер телефона обязательна',
+                      value: true,
+                    },
+                  })}
+                  placeholder="Telephone"
+                  type="tel"
+                  id="tel"
+                />
+              </div>
+            </div>
+            <label
+              htmlFor="fullname"
+              className={styles['register__form-label']}
+            >
+              Фамилия и Имя <span>*</span>:
+            </label>{' '}
+            <br />
+            <input
+              className={styles['register__form-input']}
+              id="fullname"
+              {...register('fullName', {
+                required: {
+                  message: 'Эл. почта обязательна',
+                  value: true,
+                },
+                minLength: {
+                  message: 'Минимум 10 символов',
+                  value: 10,
+                },
+                pattern: {
+                  value: /^[A-Za-zА-Яа-яЁё]+(?:\s+[A-Za-zА-Яа-яЁё]+)+$/,
+                  message:
+                    'Имя и фамилия должны состоять из букв и хотя бы из двух слов',
+                },
+              })}
+              placeholder="fullname"
+              type="text"
+            />
+            <br />
+            <label htmlFor="region" className={styles['register__form-label']}>
+              Регион <span>*</span>:
+            </label>{' '}
+            <br />
+            <input
+              className={styles['register__form-input']}
+              {...register('region', {
+                required: {
+                  message: 'Регион указать обязательна',
+                  value: true,
+                },
+              })}
+              placeholder="region"
+              type="text"
+              id="region"
+            />
+            <br />
+            <div className={styles.register__block}>
+              <label htmlFor="email" className={styles['register__form-label']}>
+                Пароль <span>*</span>:
+              </label>{' '}
+              <br />
+              <input
+                className={styles['register__form-input']}
+                {...register('password', {
+                  required: {
+                    message: 'Пароль обязателен',
+                    value: true,
+                  },
+                  pattern: {
+                    value:
+                      /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g,
+                    message:
+                      'Пароль должен содержать минимум 8 символов, заглавную букву, число!',
+                  },
+                })}
+                id="email"
+                placeholder="Придумайте пароль"
+                type={'password'}
+                autoComplete="new-password"
+              />
+            </div>
+            <br />
+            <div className={styles.register__block}>
+              <label
+                htmlFor="confirmPassword"
+                className={styles['register__form-label']}
+              >
+                Подтвердите пароль <span>*</span>:
+              </label>{' '}
+              <br />
+              <input
+                className={styles['register__form-input']}
+                {...register('confirmPassword', {
+                  required: {
+                    message: 'Подтвердите пароль',
+                    value: true,
+                  },
+                  validate: (value) =>
+                    value === getValues('password') || 'Пароли не совпадают',
+                })}
+                placeholder="Повторите пароль"
+                type="password"
+                autoComplete="new-password"
+                id="confirmPassword"
+              />
+            </div>
+            <button type="submit" className={styles['register__form-button']}>
+              Зарегистрироваться
+            </button>
+          </form>
         </div>
-
-        <button type="submit">Зарегистрироваться</button>
-      </form>
-    </>
+      </Container>
+    </div>
   )
 }
 
